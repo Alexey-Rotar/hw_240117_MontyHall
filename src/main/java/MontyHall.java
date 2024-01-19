23c114120eee66bf105d001cd7c7bc38a5bc16da
alexey.rotar <ar_mail@inbox.ru>
2024-01-19 08:00:33 +0500

    initial

---
+++ src/main/java/MontyHall.java
@@ -0,0 +1,37 @@
+import java.lang.Math;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MontyHall
+{
+    public static void main(String[] args) {
+        int DRAWS_COUNT = 1000;
+        int winDoor, firstChoice, secondChoice = 0, openedDoor = 0, wins = 0;
+        Map <Integer, Boolean> result = new HashMap<>();
+        for (int i = 1; i < DRAWS_COUNT; i++){
+            // случайное определение  выигрышной двери
+            winDoor = (int) (Math.random() * 3) + 1;
+            // случайный первый выбор
+            firstChoice = (int) (Math.random() * 3) + 1;
+            // открывается дверь, не совпадающая с выигрышной и выбранной
+            do { openedDoor ++; }
+            while(openedDoor == winDoor || openedDoor == firstChoice);
+            // второй выбор, не совпадающий с первым выбором и уже открытой дверью
+            do { secondChoice ++; }
+            while(secondChoice == firstChoice || secondChoice == openedDoor);
+            // занесение текущего результата
+            if (secondChoice == winDoor) {
+                result.put(i, true);
+            } else {
+                result.put(i, false);
+            }
+            openedDoor = 0;
+            secondChoice = 0;
+        }
+        for (int i = 1; i <= result.size(); i++){
+            if (result.get(i))
+                wins++;
+        }
+        System.out.printf("Выигрышей: %.1f%%", (float)wins/DRAWS_COUNT*100);
+    }
+}
